c0f790c5231f310919a97656d50b64fa9d7186dc
yuqisha <ericsyq97@gmail.com>
2022-05-04 17:58:08 +0800

    feat: add lint

---
+++ packages/basic/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:eslint-comments/recommended',
+    'plugin:yml/recommended',
+    'prettier',
+  ],
+  plugins: ['html'],
+  rules: {
+    
+  },
+}
